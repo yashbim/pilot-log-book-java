5d3818aa5d9352a452b8c667577d555467db37c5
yashbim <ybimsara03@gmail.com>
2024-12-07 19:06:12 +0530

    Added return function to addflight to print flight time via the main function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,19 @@
 package org.example;
-
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         String pilotID = null;
         String flightRegistration = null;
         int flightTime = 0;
-        addFlight(pilotID, flightRegistration, flightTime);
+
+//        Function to add flights
+        flightTime=addFlight(pilotID, flightRegistration, flightTime);
+
+        System.out.println("Flight time in minutes : " + flightTime);
     }
 
-    private static void addFlight(String pilotID, String flightRegistration, int flightTime) {
+    private static int addFlight(String pilotID, String flightRegistration, int flightTime) {
         Scanner myscanner = new Scanner(System.in);
         System.out.println("Enter Pilot ID");
         pilotID = myscanner.nextLine();
@@ -20,14 +21,16 @@ public class Main {
         flightRegistration = myscanner.nextLine();
         System.out.println("Enter Flight Time (Minutes)");
         flightTime = myscanner.nextInt();
-//        return pilotID, flightTime, flightRegistration;
 
-        // print input
-        System.out.println(
-                "Pilot ID : " + pilotID + "\n" +
-                        "Flight Registration : " + flightRegistration + "\n" +
-                        "Flight Time : " + flightTime);
-        System.out.println("Done");
+//        // print input
+//        System.out.println(
+//                "Pilot ID : " + pilotID + "\n" +
+//                        "Flight Registration : " + flightRegistration + "\n" +
+//                        "Flight Time : " + flightTime);
+//        System.out.println("Done");
+
+        return flightTime;
+
 
     }
 
