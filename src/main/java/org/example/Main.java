17fbf5ab8a45a66014b17ef87416ff283447f553
yashbim <ybimsara03@gmail.com>
2024-12-13 02:55:08 +0530

    added initial getters/setters (learn)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Pilot bimsara = new Pilot();
+        Pilot yashasshree = new Pilot();
 
+        bimsara.setId("b#ms@r@");
+        bimsara.setName("Bimsara");
+        bimsara.setAge(21);
+        System.out.printf(bimsara.getId());
     }
 }
---
+++ src/main/java/org/example/Pilot.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Pilot {
+    private String id;
+    private String name;
+    private int age;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
